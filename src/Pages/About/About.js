f9e6e44730a762ebf40c1521e6241d11a87a690d
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-11-11 10:39:25 +0600

    first commit

---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>this is our about</h1>
+            
+        </div>
+    );
+};
+
+export default About;
