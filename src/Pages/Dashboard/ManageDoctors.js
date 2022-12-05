fc46263919dfa7f0ea55d55d4372201214600d07
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-12-05 22:52:07 +0600

    commit

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import { useQueries, useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const {data:doctors, isLoading} = useQuery('doctors',()=> fetch('http://localhost:5000/doctor',{
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
 
-        headers:{
+        headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }).then(res=>res.json()))
-    if(isLoading){
+    }).then(res => res.json()))
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div>
             <h2>manage doctor:{doctors.length}</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._key}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                            ></DoctorRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
