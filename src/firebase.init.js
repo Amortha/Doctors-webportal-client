ce45378c5996d78a7136172177014d32f8b32826
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-12-14 20:09:05 +0600

    commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAN2T9413cV94hKN348cTt-8V5jgAH5ZF8",
+  authDomain: "doctors-web-portal-1a97a.firebaseapp.com",
+  projectId: "doctors-web-portal-1a97a",
+  storageBucket: "doctors-web-portal-1a97a.appspot.com",
+  messagingSenderId: "428442518746",
+  appId: "1:428442518746:web:6070e1e70e45c13c9411e3"
 
 
   // apiKey: process.env.REACT_APP_API_KEY,
